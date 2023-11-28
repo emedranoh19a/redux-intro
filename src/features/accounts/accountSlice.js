aef226473fa201f43c2fa8826947ee7a68cb596d
user <user@usernoMacBook-Pro.local>
2023-11-28 17:48:04 +0900

    added Slice in the RTK way

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -19,11 +19,16 @@ const accountSlice = createSlice({
     withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan > 0) return;
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance += action.payload.amount;
+    requestLoan: {
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
     },
     payLoan(state, action) {
       state.balance -= state.loan;
@@ -32,6 +37,5 @@ const accountSlice = createSlice({
     },
   },
 });
-console.log(accountSlice);
 export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
 export default accountSlice.reducer;
